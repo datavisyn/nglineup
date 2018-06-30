0583bceb423f20327eb473f216117a135d2d3f32
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-06-30 22:34:36 +0200

    fix exports

--- src/public_api.ts
+++ src/public_api.ts
@@ -4,4 +4,6 @@
 
 export * from './lib/lineup.service';
 export * from './lib/lineup.component';
+export * from './lib/lineup-ranking.component';
+export * from './lib/lineup-column.component';
 export * from './lib/lineup.module';
